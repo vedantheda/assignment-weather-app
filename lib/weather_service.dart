bd474ad528bf16dce9f14c755ebfc2c847de9906
vedantheda <vedant.heda@outlook.com>
2023-12-12 17:44:22 +0530

    Update weather_service.dart

--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -6,11 +6,13 @@ class WeatherService {
       'bb008bd8ea87985994e22f7fa22053f2'; // I can use .env to hide this API key but have not done it for the assignment.
 
   Future<Map<String, dynamic>> getWeather(String city) async {
+    // using http module to fetch the weather data from the API.
     final url =
         'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
+      // Decoding response body and returnong the data.
       final data = json.decode(response.body);
       return {
         'temperature': data['main']['temp'],
